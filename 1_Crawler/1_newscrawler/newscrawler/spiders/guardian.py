31899490de0a5e676c3590c8f0c88c7b554975c2
asuerdem <33291645+asuerdem@users.noreply.github.com>
2017-11-18 20:52:12 +0000

    Update guardian.py

--- 1_Crawler/1_newscrawler/newscrawler/spiders/guardian.py
+++ 1_Crawler/1_newscrawler/newscrawler/spiders/guardian.py
@@ -22,7 +22,7 @@ class GuardianSpider(CrawlSpider):
         '06':'jun', '07':'jul', '08':'aug', '09':'sep', '10':'oct', '11':'nov', '12':'dec' }
         months = [month_dict[ re.findall('[0-9]{4}/([0-9]{2})/[0-9]{2}',d)[0] ] for d in date_inds]
         date_inds = [re.sub('/[0-9]{2}/',"/" + month_dict[re.findall('[0-9]{4}/([0-9]{2})/[0-9]{2}',d)[0] ] + "/",d)  for d in date_inds]
-        self.start_urls = ["https://www.theguardian.com/technology/%s" % d for d in date_inds] # This will be the list of archive pages
+        self.start_urls = ["https://www.theguardian.com/technology/%s/all" % d for d in date_inds] # This will be the list of archive pages
         #needs updating according to the following format: https://www.theguardian.com/world/2015/nov/09/all
         
         
