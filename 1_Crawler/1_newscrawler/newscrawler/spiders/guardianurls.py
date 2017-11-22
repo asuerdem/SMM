26bba63a7abaa76f12850cdf5d16ed6f9234d1c2
fuadcan <32942249+fuadcan@users.noreply.github.com>
2017-11-22 16:35:26 +0300

    Update guardianurls.py

--- 1_Crawler/1_newscrawler/newscrawler/spiders/guardianurls.py
+++ 1_Crawler/1_newscrawler/newscrawler/spiders/guardianurls.py
@@ -17,13 +17,14 @@ class GuardianSpider(CrawlSpider):
         #  The archive pages that we usually generate by using Excel
         super(GuardianSpider, self).__init__(*args, **kwargs)
         begin_date = pd.Timestamp(yearmonth + "-01")
-        end_date = pd.Timestamp(begin_date) + pd.DateOffset(months=0) + pd.DateOffset(days=1)
+        end_date = pd.Timestamp(begin_date) + pd.DateOffset(months=1) - pd.DateOffset(days=1)
         date_inds  = [d.date().isoformat().replace("-","/") for d in pd.date_range(begin_date,end_date)]
         month_dict = {'01':'jan', '02':'feb', '03':'mar', '04':'april', '05':'may',
         '06':'jun', '07':'jul', '08':'aug', '09':'sep', '10':'oct', '11':'nov', '12':'dec' }
         months = [month_dict[ re.findall('[0-9]{4}/([0-9]{2})/[0-9]{2}',d)[0] ] for d in date_inds]
         date_inds = [re.sub('/[0-9]{2}/',"/" + month_dict[re.findall('[0-9]{4}/([0-9]{2})/[0-9]{2}',d)[0] ] + "/",d)  for d in date_inds]
-        category_list = ["technology", "science"]
+        # category_list = ["technology", "science"]
+        category_list = pd.read_table('../guardiansections.txt')['Sections'].values.tolist()
         urls_list = [["https://www.theguardian.com/%s/%s/all" % (c,d) for c in category_list] for d in date_inds]
         self.start_urls = sum(urls_list,[]) # This will be the list of archive pages
         #needs updating according to the following format: https://www.theguardian.com/world/2015/nov/09/all
@@ -41,13 +42,18 @@ class GuardianSpider(CrawlSpider):
         article = NewsPlease.from_url(item["link"])
         item["lang"]   = "en"
         item["source"] = "guardian"
-        topic = hxs.xpath('//ul[@class="submeta__links"]//a/text()').extract()
-        topic = [t.strip() for t in topic]
-        item['topic']   = '|'.join(topic)
+        ### The following lines does not change amongst newspapers, BEGIN ###
         item['title']   = article.title
         item['intro']   = article.description
         item["author"]  = '|'.join(article.authors)
         item["content"] = article.text
         item["date_time"] = article.date_publish.isoformat()
-        item["category"]  = ''
+        ### END ###
+        topic = hxs.xpath('//ul[@class="submeta__links"]//a/text()').extract()
+        topic = [t.strip() for t in topic]
+        item['topic']   = '|'.join(topic)
+        category = hxs.xpath('//ul[@class="signposting"]//a/text()').extract()
+        category           = category[1:]
+        category = [c for c in category if not c==">"]
+        item["category"]   = '|'.join(category)
         return(item)
