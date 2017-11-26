64d450aa05547c85ece24280368cce6bfaa88373
fuadcan <fcbeylunioglu@gmail.com>
2017-11-26 21:09:01 +0300

    crawler is updated

--- 1_Crawler/1_newscrawler/newscrawler/spiders/peopleschn.py
+++ 1_Crawler/1_newscrawler/newscrawler/spiders/peopleschn.py
@@ -15,13 +15,14 @@ class PeoplesChinaSpider(CrawlSpider):
     def __init__(self, yearmonth='', *args, **kwargs):
         super(PeoplesChinaSpider, self).__init__(*args, **kwargs)
         begin_date = pd.Timestamp(yearmonth + "-01")
-        end_date = pd.Timestamp(begin_date) + pd.DateOffset(months=0) + pd.DateOffset(days=1)
+        end_date = pd.Timestamp(begin_date) + pd.DateOffset(months=0) + pd.DateOffset(days=0)
         date_inds  = [d.date().isoformat().replace("-","") for d in pd.date_range(begin_date,end_date)]
         self.start_urls = ["http://en.people.cn/review/%s.html" % d for d in date_inds]
 
     rules = (
         Rule(LinkExtractor(allow=(), restrict_xpaths=('//div[@class="p1_left fl"]//a',
-        '//div[@class="p1_right fr"]//a','//div[@class="p1_c fl"]//a','//div[@class="p1_c2 fl"]//a',),deny=('//div[@class="ad02 clear"]/a')), callback="parse_items", follow= True),
+        '//div[@class="p1_right fr"]//a','//div[@class="p2_left fl"]//a',
+        '//div[@class="p1_c fl"]//a','//div[@class="p1_c2 fl"]//a',),deny=('//div[@class="ad02 clear"]/a')), callback="parse_items", follow= False),
     )
 
     def parse_items(self, response):
